8841ce692e58029030a826bcb6ef6460e0f38a7d
Atul Varma <avarma@mozilla.com>
2010-06-16 20:16:17 -0700

    added mongo_storage module, server.py and example client HTML file.

---
+++ mozsummit/mongo_storage.py
@@ -0,0 +1,50 @@
+import datetime
+
+class MongoStorage(object):
+    '''
+    Note that this example code assumes a MongoDB server is active on
+    localhost at the default port.
+
+      >>> import pymongo
+      >>> conn = pymongo.Connection()
+      >>> coll = conn.mozsummit_test_database.storage_test
+      >>> coll.remove()
+
+      >>> s = MongoStorage(coll)
+      >>> 'blah' in s
+      False
+      >>> s['blah'] = {'foo': 1}
+      >>> 'blah' in s
+      True
+
+      >>> s['blah']
+      {u'foo': 1}
+      >>> del s['blah']
+      >>> 'blah' in s
+      False
+    '''
+
+    def __init__(self, collection):
+        self.collection = collection
+        self.collection.ensure_index('name')
+
+    def __contains__(self, name):
+        doc = self.collection.find_one({'name': name})
+        return (doc is not None)
+
+    def __delitem__(self, name):
+        if not name in self:
+            raise KeyError(name)
+        self.collection.remove({'name': name})
+
+    def __getitem__(self, name):
+        if not name in self:
+            raise KeyError(name)
+        return self.collection.find_one({'name': name})['value']
+
+    def __setitem__(self, name, value):
+        self.collection.update({'name': name},
+                               {'name': name,
+                                'value': value,
+                                'date': datetime.datetime.utcnow()},
+                               upsert=True)
---
+++ server.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import json
+
+import oauth2 as oauth
+import pymongo
+from mozsummit.api import MozSummitApi
+from mozsummit.mongo_storage import MongoStorage
+from mozsummit.twitter_client import TwitterOauthClientApp
+
+try:
+    conn = pymongo.Connection()
+except Exception, e:
+    print('Running this app requires a MongoDB server '
+          'to be active on localhost at the default port.')
+    sys.exit(1)
+
+CONFIG_FILE = "config.json"
+
+CONFIG_DOCS = {
+    'db': 'name of the MongoDB database to use for storage',
+    'consumer_key': 'OAuth consumer key for Twitter',
+    'consumer_secret': 'OAuth consumer secret for Twitter'
+    }
+
+if not os.path.exists(CONFIG_FILE):
+    print("%s not found. Please create a JSON-formatted file with "
+          "this name and the following keys:\n" % CONFIG_FILE)
+    for name, text in CONFIG_DOCS.items():
+        print "  %-15s - %s" % (name, text)
+    print
+    sys.exit(1)
+
+config = json.loads(open(CONFIG_FILE).read())
+
+missing_keys = [name for name in CONFIG_DOCS
+                if name not in config]
+
+if missing_keys:
+    print("Your %s file does not contain the following "
+          "keys:\n" % CONFIG_FILE)
+    for name in missing_keys:
+        print "  %-15s - %s" % (name, CONFIG_DOCS[name])
+    print
+    sys.exit(1)
+
+db = conn[config['db']]
+
+consumer = oauth.Consumer(config['consumer_key'],
+                          config['consumer_secret'])
+
+twitter = TwitterOauthClientApp(
+    consumer=consumer,
+    oauth=oauth,
+    request_tokens=MongoStorage(db.twitter_oauth_request_tokens)
+    )
+
+api = MozSummitApi(twitter=twitter, db=db)
+
+app = api.wsgi_app
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    
+    port = 8000
+    httpd = make_server('', port, app)
+    print "serving on port %d" % port
+    httpd.serve_forever()
