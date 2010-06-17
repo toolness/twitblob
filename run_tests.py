e67d53777a45d019c0949b389b78c3b582d66a5c
Atul Varma <avarma@mozilla.com>
2010-06-16 20:25:11 -0700

    Added test for pymongo in run_tests so they don't fail silently.

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,16 @@
 
 if __name__ == '__main__':
     import nose
+    import sys
+    import pymongo
+
+    try:
+        conn = pymongo.Connection()
+        conn.end_request()
+    except Exception, e:
+        print('Running this test suite requires a MongoDB server '
+              'to be active on localhost at the default port.')
+        sys.exit(1)
 
     nose.run(env={'NOSE_WITH_DOCTEST': 1,
                   'NOSE_DOCTEST_TESTS': 1})
