3539dda999ff0c119ef31173d03e620d8ce78be6
Atul Varma <avarma@mozilla.com>
2010-06-16 22:50:40 -0700

    replaced 'import json' -> 'import simplejson as json' for py2.5 compat

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import json
+import simplejson as json
 
 import oauth2 as oauth
 import pymongo
--- twitblob/twitter_client.py
+++ twitblob/twitter_client.py
@@ -1,4 +1,4 @@
-import json
+import simplejson as json
 import urlparse
 import urllib
 from wsgiref.util import application_uri
