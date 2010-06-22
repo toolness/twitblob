532cf86b91d296903da61ebc18593bb65a84bde1
Atul Varma <avarma@mozilla.com>
2010-06-21 20:58:05 -0700

    refactored new Request object.

--- twitblob/api.py
+++ twitblob/api.py
@@ -23,6 +23,112 @@ def gentoken():
     # an annoying '=' in the string.
     return urlsafe_b64encode(urandom(256/8+1))
 
+class Request(object):
+    def __init__(self, api, environ, start_response):
+        self.api = api
+        self.environ = environ
+        self.start_response = start_response
+        self.path = environ['PATH_INFO']
+        self.method = environ['REQUEST_METHOD']
+        self.qargs = dict(parse_qsl(self.environ.get('QUERY_STRING', '')))
+
+        try:
+            self.length = int(environ.get('CONTENT_LENGTH', '0'))
+        except ValueError:
+            self.length = 0
+
+    def json_response(self, status, obj):
+        self.start_response(status,
+                            [('Content-Type', 'application/json')])
+        return [json.dumps(obj)]
+
+    def get_body(self):
+        f = self.environ['wsgi.input']
+        try:
+            obj = json.loads(f.read(self.length))
+        except ValueError:
+            return (None, None)
+        if 'token' in obj:
+            token = self.api.db.auth_tokens.find_one({'id': obj['token']})
+            if token is None:
+                return (obj, None)
+            if self.api.utcnow() - token['date'] > self.api.token_lifetime:
+                token = None
+                self.api.db.auth_tokens.remove({'id': obj['token']})
+            return (obj, token)
+        else:
+            return (obj, None)
+
+    def serve_blob(self):
+        if self.path == '/blobs/':
+            if 'ids' in self.qargs:
+                try:
+                    ids = [int(strid)
+                           for strid in self.qargs['ids'].split(",")]
+                    blobs = {}
+                    # TODO: This may be really slow because of the
+                    # many requests we're making to the DB. Shouldn't
+                    # be hard to optimize, though.
+                    for intid in ids:
+                        blob = self.api.db.blobs.find_one({'user_id': intid})
+                        if blob is not None:
+                            blobs[blob['screen_name']] = blob['data']
+                    return self.json_response('200 OK', blobs)
+                except ValueError:
+                    return self.json_response('400 Bad Request',
+                                              {'error': 'invalid ids'})
+            return self.json_response('400 Bad Request',
+                                      {'error': 'need query args'})
+        user = self.path.split('/')[2]
+        if self.method == 'POST':
+            obj, token = self.get_body()
+            if obj is None:
+                return self.json_response(
+                    '400 Bad Request',
+                    {'error': 'error parsing JSON body'}
+                    )
+            if not (isinstance(obj, dict) and 
+                    isinstance(obj.get('data'), dict)):
+                return self.json_response(
+                    '400 Bad Request',
+                    {'error': 'body must contain "data" object'}
+                    )
+            if token and token['screen_name'] == user:
+                self.api.db.blobs.update({'user_id': token['user_id']},
+                                         {'screen_name': user,
+                                          'user_id': token['user_id'],
+                                          'data': obj['data']},
+                                         upsert=True)
+                return self.json_response('200 OK', {'success': True})
+            else:
+                return self.json_response(
+                    '403 Forbidden',
+                    {'error': 'Missing or invalid auth token'}
+                    )
+        elif self.method == 'GET':
+            blob = self.api.db.blobs.find_one({'screen_name': user})
+            if blob is None:
+                return self.json_response('404 Not Found',
+                                          {'error': 'blob does not exist'})
+            return self.json_response('200 OK', blob['data'])
+
+    def process(self):
+        if self.path.startswith('/login/'):
+            wsgiref.util.shift_path_info(self.environ)
+            return self.api.twitter(self.environ,
+                                    self.start_response)
+
+        if self.length > self.api.max_body_size:
+            return self.json_response('413 Request Entity Too Large',
+                                      {'error': 'too big'})
+
+        if self.path.startswith('/blobs/'):
+            return self.serve_blob()
+
+        self.start_response('404 Not Found',
+                            [('Content-Type', 'text/plain')])
+        return ['unknown path: %s' % self.path]
+
 class TwitBlobApi(object):
     def __init__(self, twitter, db, max_body_size=DEFAULT_MAX_BODY_SIZE,
                  token_lifetime=DEFAULT_TOKEN_LIFETIME,
@@ -65,101 +171,5 @@ class TwitBlobApi(object):
 
     @allow_cross_origin
     def wsgi_app(self, environ, start_response):
-        path = environ['PATH_INFO']
-        method = environ['REQUEST_METHOD']
-
-        if path.startswith('/login/'):
-            wsgiref.util.shift_path_info(environ)
-            return self.twitter(environ, start_response)
-
-        def json_response(status, obj):
-            start_response(status,
-                           [('Content-Type', 'application/json')])
-            return [json.dumps(obj)]
-
-        def err_404():
-            start_response('404 Not Found',
-                           [('Content-Type', 'text/plain')])
-            return ['unknown path: %s' % path]
-
-        try:
-            length = int(environ.get('CONTENT_LENGTH', '0'))
-        except ValueError:
-            length = 0
-
-        if length > self.max_body_size:
-            return json_response('413 Request Entity Too Large',
-                                 {'error': 'too big'})
-
-        def get_body():
-            f = environ['wsgi.input']
-            try:
-                obj = json.loads(f.read(length))
-            except ValueError:
-                return (None, None)
-            if 'token' in obj:
-                token = self.db.auth_tokens.find_one({'id': obj['token']})
-                if token is None:
-                    return (obj, None)
-                if self.utcnow() - token['date'] > self.token_lifetime:
-                    token = None
-                    self.db.auth_tokens.remove({'id': obj['token']})
-                return (obj, token)
-            else:
-                return (obj, None)
-
-        if path.startswith('/blobs/'):
-            if path == '/blobs/':
-                qargs = dict(parse_qsl(environ.get('QUERY_STRING', '')))
-                if 'ids' in qargs:
-                    try:
-                        ids = [int(strid)
-                               for strid in qargs['ids'].split(",")]
-                        blobs = {}
-                        # TODO: This may be really slow because of the
-                        # many requests we're making to the DB. Shouldn't
-                        # be hard to optimize, though.
-                        for intid in ids:
-                            blob = self.db.blobs.find_one({'user_id': intid})
-                            if blob is not None:
-                                blobs[blob['screen_name']] = blob['data']
-                        return json_response('200 OK', blobs)
-                    except ValueError:
-                        return json_response('400 Bad Request',
-                                             {'error': 'invalid ids'})
-                return json_response('400 Bad Request',
-                                     {'error': 'need query args'})
-            user = path.split('/')[2]
-            if method == 'POST':
-                obj, token = get_body()
-                if obj is None:
-                    return json_response(
-                        '400 Bad Request',
-                        {'error': 'error parsing JSON body'}
-                        )
-                if not (isinstance(obj, dict) and 
-                        isinstance(obj.get('data'), dict)):
-                    return json_response(
-                        '400 Bad Request',
-                        {'error': 'body must contain "data" object'}
-                        )
-                if token and token['screen_name'] == user:
-                    self.db.blobs.update({'user_id': token['user_id']},
-                                         {'screen_name': user,
-                                          'user_id': token['user_id'],
-                                          'data': obj['data']},
-                                         upsert=True)
-                    return json_response('200 OK', {'success': True})
-                else:
-                    return json_response(
-                        '403 Forbidden',
-                        {'error': 'Missing or invalid auth token'}
-                        )
-            elif method == 'GET':
-                blob = self.db.blobs.find_one({'screen_name': user})
-                if blob is None:
-                    return json_response('404 Not Found',
-                                         {'error': 'blob does not exist'})
-                return json_response('200 OK', blob['data'])
-
-        return err_404()
+        request = Request(self, environ, start_response)
+        return request.process()
