cb2f77d1b1a3b322801e8b90bef082b01ed2d361
Atul Varma <avarma@mozilla.com>
2010-06-28 09:02:10 -0700

    added kwargs to easy.make_wsgi_app.

--- twitblob/easy.py
+++ twitblob/easy.py
@@ -3,7 +3,7 @@ from twitblob.api import TwitBlobApi
 from twitblob.mongo_storage import MongoStorage
 from twitblob.twitter_client import TwitterOauthClientApp
 
-def make_wsgi_app(conn, db_name, consumer_key, consumer_secret):
+def make_wsgi_app(conn, db_name, consumer_key, consumer_secret, **kwargs):
     db = conn[db_name]
 
     consumer = oauth.Consumer(consumer_key, consumer_secret)
@@ -14,6 +14,6 @@ def make_wsgi_app(conn, db_name, consumer_key, consumer_secret):
         request_tokens=MongoStorage(db.twitter_oauth_request_tokens)
         )
 
-    api = TwitBlobApi(twitter=twitter, db=db)
+    api = TwitBlobApi(twitter=twitter, db=db, **kwargs)
 
     return api.wsgi_app
